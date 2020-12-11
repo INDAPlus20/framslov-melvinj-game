0d0cd9d86f8101fc7b1a8012e757a208a45fa84e
melvinj <13897+melvinj@users.noreply.gits-15.sys.kth.se>
2020-12-11 15:03:55 +0100

    Add basic scripting functionality

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,19 @@ use rand;
 use std::env;
 use std::path;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
+use libloading::{Library, Symbol};
+use std::fs::{self};
+use std::path::Path;
+
+pub mod structs;
+pub use structs::*;
+
+type TestFunc = unsafe fn(i32, i32) -> i32;
+type AIFunc = unsafe fn(&GameState) -> &InputState;
+
 type Point2 = na::Point2<f32>;
 type Vector2 = na::Vector2<f32>;
 
@@ -117,11 +130,11 @@ const PLAYER_TURN_RATE: f32 = 3.0;
 /// Refire delay between shots, in seconds.
 const PLAYER_SHOT_TIME: f32 = 0.5;
 
-    /// TODO 2D input based on player ID
+/// TODO 2D input based on player ID
 
 fn player_handle_input(actor: &mut PhysObject, input: &InputState, dt: f32) {
     actor.facing += dt * PLAYER_TURN_RATE * input.xaxis;
-
+    
     if input.yaxis > 0.0 {
         player_thrust(actor, dt);
     }
@@ -135,7 +148,7 @@ fn player_thrust(actor: &mut Actor, dt: f32) {
 
 const MAX_PHYSICS_VEL: f32 = 250.0;
 
-    /// TODO Update position
+/// TODO Update position
 
 fn update_physobject_position(actor: &mut PhysObject, dt: f32) {
     // Clamp the velocity to the max efficiently
@@ -184,7 +197,7 @@ fn world_to_screen_coords(screen_width: f32, screen_height: f32, point: Point2) 
 /// just hard-coded.
 /// **********************************************************************
 
-    /// TODO Handle assets
+/// TODO Handle assets
 
 struct Assets {
     player_image: graphics::Image,
@@ -201,10 +214,10 @@ impl Assets {
         let shot_image = graphics::Image::new(ctx, "/shot.png")?;
         let rock_image = graphics::Image::new(ctx, "/rock.png")?;
         let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf")?;
-
+        
         let shot_sound = audio::Source::new(ctx, "/pew.ogg")?;
         let hit_sound = audio::Source::new(ctx, "/boom.ogg")?;
-
+        
         Ok(Assets {
             player_image,
             shot_image,
@@ -214,7 +227,7 @@ impl Assets {
             hit_sound,
         })
     }
-
+    
     fn actor_image(&mut self, actor: &Actor) -> &mut graphics::Image {
         match actor.tag {
             ActorType::Player => &mut self.player_image,
@@ -235,10 +248,11 @@ impl Assets {
 /// this small it hardly matters.
 /// **********************************************************************
 
-    /// Mainstate
+/// Mainstate
 
 struct MainState {
     player: Actor,
+    player2: Actor,
     shots: Vec<Actor>,
     rocks: Vec<Actor>,
     level: i32,
@@ -247,22 +261,26 @@ struct MainState {
     screen_width: f32,
     screen_height: f32,
     input: InputState,
+    input2: InputState,
     player_shot_timeout: f32,
+    script1: Option<String>,
 }
 
 impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
+    fn new(ctx: &mut Context, script_name: Option<String>) -> GameResult<MainState> {
         println!("Game resource path: {:?}", ctx.filesystem);
-
+        
         print_instructions();
-
+        
         let assets = Assets::new(ctx)?;
         let player = create_player();
+        let player2 = create_player();
         let rocks = create_rocks(5, player.pos, 100.0, 250.0);
-
+        
         let (width, height) = graphics::drawable_size(ctx);
         let s = MainState {
             player,
+            player2,
             shots: Vec::new(),
             rocks,
             level: 0,
@@ -271,14 +289,16 @@ impl MainState {
             screen_width: width,
             screen_height: height,
             input: InputState::default(),
+            input2: InputState::default(),
             player_shot_timeout: 0.0,
+            script1: script_name,
         };
-
+        
         Ok(s)
     }
-
+    
     /// TODO Collisions
-
+    
     fn handle_collisions(&mut self) {
         for rock in &mut self.rocks {
             let pdistance = rock.pos - self.player.pos;
@@ -291,13 +311,13 @@ impl MainState {
                     shot.life = 0.0;
                     rock.life = 0.0;
                     self.score += 1;
-
+                    
                     let _ = self.assets.hit_sound.play();
                 }
             }
         }
     }
-
+    
     fn check_for_level_respawn(&mut self) {
         if self.rocks.is_empty() {
             self.level += 1;
@@ -320,7 +340,7 @@ fn print_instructions() {
     println!();
 }
 
-    /// TODO Draw
+/// TODO Draw
 
 fn draw_physobject(
     assets: &mut Assets,
@@ -332,9 +352,9 @@ fn draw_physobject(
     let pos = world_to_screen_coords(screen_w, screen_h, actor.pos);
     let image = assets.actor_image(actor);
     let drawparams = graphics::DrawParam::new()
-        .dest(pos)
-        .rotation(actor.facing as f32)
-        .offset(Point2::new(0.5, 0.5));
+    .dest(pos)
+    .rotation(actor.facing as f32)
+    .offset(Point2::new(0.5, 0.5));
     graphics::draw(ctx, image, drawparams)
 }
 
@@ -346,17 +366,20 @@ fn draw_physobject(
 impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         const DESIRED_FPS: u32 = 60;
-
+        
         while timer::check_update_time(ctx, DESIRED_FPS) {
             let seconds = 1.0 / (DESIRED_FPS as f32);
-
+            
+            self.input2 = ai_generate_input(&self.gamestate);
+            ai_handle_input(&mut self.player2);
+            
             // Update the player state based on the user input.
             player_handle_input(&mut self.player, &self.input, seconds);
             self.player_shot_timeout -= seconds;
             if self.input.fire && self.player_shot_timeout < 0.0 {
                 self.fire_player_shot();
             }
-
+            
             // Update the physics for all actors.
             // First the player...
             update_actor_position(&mut self.player, seconds);
@@ -365,28 +388,28 @@ impl EventHandler for MainState {
                 self.screen_width as f32,
                 self.screen_height as f32,
             );
-
+            
             // Then the shots...
             for act in &mut self.shots {
                 update_actor_position(act, seconds);
                 wrap_actor_position(act, self.screen_width as f32, self.screen_height as f32);
                 handle_timed_life(act, seconds);
             }
-
+            
             // And finally the rocks.
             for act in &mut self.rocks {
                 update_actor_position(act, seconds);
                 wrap_actor_position(act, self.screen_width as f32, self.screen_height as f32);
             }
-
+            
             // Handle the results of things moving:
             // collision detection, object death, and if
             // we have killed all the rocks in the level,
             // spawn more of them.
             self.handle_collisions();
-
+            
             self.check_for_level_respawn();
-
+            
             // Finally we check for our end state.
             // I want to have a nice death screen eventually,
             // but for now we just quit.
@@ -395,46 +418,46 @@ impl EventHandler for MainState {
                 let _ = event::quit(ctx);
             }
         }
-
+        
         Ok(())
     }
-
+    
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         // Our drawing is quite simple.
         // Just clear the screen...
         graphics::clear(ctx, graphics::BLACK);
-
+        
         // Loop over all objects drawing them...
         {
             let assets = &mut self.assets;
             let coords = (self.screen_width, self.screen_height);
-
+            
             let p = &self.player;
             draw_physobject(assets, ctx, p, coords)?;
-
+            
             for s in &self.shots {
                 draw_physobject(assets, ctx, s, coords)?;
             }
-
+            
             for r in &self.rocks {
                 draw_physobject(assets, ctx, r, coords)?;
             }
         }
-
+        
         // And draw the GUI elements in the right places.
         let level_dest = Point2::new(10.0, 10.0);
         let score_dest = Point2::new(200.0, 10.0);
-
+        
         let level_str = format!("Level: {}", self.level);
         let score_str = format!("Score: {}", self.score);
         let level_display = graphics::Text::new((level_str, self.assets.font, 32.0));
         let score_display = graphics::Text::new((score_str, self.assets.font, 32.0));
         graphics::draw(ctx, &level_display, (level_dest, 0.0, graphics::WHITE))?;
         graphics::draw(ctx, &score_display, (score_dest, 0.0, graphics::WHITE))?;
-
+        
         // Then we flip the screen...
         graphics::present(ctx)?;
-
+        
         // And yield the timeslice
         // This tells the OS that we're done using the CPU but it should
         // get back to this program as soon as it can.
@@ -444,9 +467,9 @@ impl EventHandler for MainState {
         timer::yield_now();
         Ok(())
     }
-
+    
     /// TODO Keyboard events
-
+    
     // Handle key events.  These just map keyboard events
     // and alter our input state appropriately.
     fn key_down_event(
@@ -472,13 +495,13 @@ impl EventHandler for MainState {
             KeyCode::P => {
                 let img = graphics::screenshot(ctx).expect("Could not take screenshot");
                 img.encode(ctx, graphics::ImageFormat::Png, "/screenshot.png")
-                    .expect("Could not save screenshot");
+                .expect("Could not save screenshot");
             }
             KeyCode::Escape => event::quit(ctx),
             _ => (), // Do nothing
         }
     }
-
+    
     fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods) {
         match keycode {
             KeyCode::Up => {
@@ -495,12 +518,76 @@ impl EventHandler for MainState {
     }
 }
 
+fn test_plugin(a: i32, b: i32, name: &str) -> i32 {
+    let lib = Library::new(name.replace("rs","dll")).unwrap();
+    unsafe {
+        let func: Symbol<TestFunc> = lib.get(b"calculate_move").unwrap();
+        let answer = func(a, b);
+        answer
+    }
+}
+
+fn ai_generate_input(gamestate: &mut GameState, name: &str) -> &mut InputState {
+    let lib = Library::new(name.replace("rs","dll")).unwrap();
+    unsafe {
+        let func: Symbol<AIFunc> = lib.get(b"calculate_move").unwrap();
+        let input = func(gamestate);
+        input
+    }
+}
+
+fn compile_file(path: &Path) {
+    let mut compile_file = Command::new("cmd");
+    compile_file.args(&["/C", "rustc", "--crate-type", "cdylib", path.as_os_str().to_str().unwrap()]).status().expect("process failed to execute");
+}
+
 /// **********************************************************************
 /// Finally our main function!  Which merely sets up a config and calls
 /// `ggez::event::run()` with our `EventHandler` type.
 /// **********************************************************************
 
 pub fn main() -> GameResult {
+    //AI script loading
+    let paths = fs::read_dir("src/script/").unwrap();
+    
+    //Name of selected script with functioning test function
+    let maybe_name: Option<String> = None;
+    println!("Reading files from script folder:");
+
+    //Iterate over paths
+    for path_prewrap in paths {
+        let path = path_prewrap.unwrap().path();
+        
+        println!("PreFilter: {}", path.file_name().unwrap().to_str().unwrap());
+        
+        //Has to be
+        //* Not directory
+        //* Not the structs-file
+        //* Extension is .rs
+        if path.is_dir() {
+            continue
+        }
+        if path.file_name().unwrap() == "structs.rs" {
+            continue
+        }
+        if path.extension().unwrap() != "rs" {
+            continue
+        }
+
+        //All pre-requirements met
+        //Compile and test the script
+        compile_file(path.as_path());
+        println!("FileName: {}", path.file_name().unwrap().to_str().unwrap());
+        println!("Name: {}", path.display());
+        
+        //Tests the code, 1 + 3 = 4. Mostly to check connectivity
+        println!("{}+{}:{}",1,3,test_plugin(1,3, path.file_name().unwrap().to_str().unwrap()));
+
+        //Not huge error yet, store the script file name
+        maybe_name = Some(path.file_name().unwrap().to_os_string().into_string().unwrap());
+    }
+    
+    
     // We add the CARGO_MANIFEST_DIR/resources to the resource paths
     // so that ggez will look in our cargo project directory for files.
     let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
@@ -510,14 +597,14 @@ pub fn main() -> GameResult {
     } else {
         path::PathBuf::from("./resources")
     };
-
+    
     let cb = ContextBuilder::new("astroblasto", "ggez")
-        .window_setup(conf::WindowSetup::default().title("Astroblasto!"))
-        .window_mode(conf::WindowMode::default().dimensions(640.0, 480.0))
-        .add_resource_path(resource_dir);
-
+    .window_setup(conf::WindowSetup::default().title("Astroblasto!"))
+    .window_mode(conf::WindowMode::default().dimensions(640.0, 480.0))
+    .add_resource_path(resource_dir);
+    
     let (ctx, events_loop) = &mut cb.build()?;
-
-    let game = &mut MainState::new(ctx)?;
+    
+    let game = &mut MainState::new(ctx, maybe_name)?;
     event::run(ctx, events_loop, game)
 }
---
+++ src/script/structs.rs
@@ -0,0 +1,24 @@
+pub mod structs;
+
+//Structs in separate file
+//Copied into script-folder for easy peasy scripting
+pub struct InputState {
+    pub placeholder_a: isize
+}
+
+pub struct GameState {
+    /*pub neutral_balls: Vec<Ball>,
+    pub enemy_balls: Vec<Ball>,
+    pub friendly_balls: Vec<Ball>,
+    pub enemy: Character,
+    pub character: Character,*/
+    pub placeholder_a: isize
+}
+
+pub struct Character {
+    pub placeholder_a: isize
+}
+
+pub struct Ball {
+    pub placeholder_a: isize
+}
